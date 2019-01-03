14389fbb23f4d70fafbca0c14f7bc7fa48e8ac8b
Welles Robinson <wir963@gmail.com>
2019-01-03 14:59:18 -0500

    more combinations of HR genes

--- TCGA-data/src/combine_hr_inactivations.py
+++ TCGA-data/src/combine_hr_inactivations.py
@@ -11,6 +11,10 @@ if __name__ == '__main__':
     silencing_df = silencing_df[genes]
     bi_inactivation_df = bi_inactivation_df[genes]
     feature_df = bi_inactivation_df + silencing_df
-    feature_df["BRCA12"] = feature_df["BRCA1"] + feature_df["BRCA2"]
+    feature_df["BRCA1&BRCA2"] = feature_df["BRCA1"] + feature_df["BRCA2"]
+    feature_df["BRCA1&BRCA2&RAD51C"] = feature_df["BRCA1&BRCA2"] + feature_df["RAD51C"]
+    feature_df["BRCA1&BRCA2&RAD51C&ATM"] = feature_df["BRCA1&BRCA2&RAD51C"] + feature_df["ATM"]
+    feature_df["BRCA1&BRCA2&RAD51C&FANCM"] = feature_df["BRCA1&BRCA2&RAD51C"] + feature_df["FANCM"]
+    feature_df["BRCA1&BRCA2&RAD51C&FANCF"] = feature_df["BRCA1&BRCA2&RAD51C"] + feature_df["FANCF"]
     feature_df = feature_df.sort_index()
     feature_df.to_csv(snakemake.output[0], sep="\t")
