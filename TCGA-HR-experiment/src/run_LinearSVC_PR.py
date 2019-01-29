e3d40830a5f8bbd683f86ad41b4d091f3824f3fd
Welles Robinson <wir963@gmail.com>
2019-01-29 18:12:38 -0500

    move prediction to TCGA-HR directory

--- TCGA-HR-experiment/src/run_LinearSVC_PR.py
+++ TCGA-HR-experiment/src/run_LinearSVC_PR.py
@@ -2,8 +2,19 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
 from statistics import mean
-import matplotlib.pyplot as plt
-from linearSVCPR import pr_linear_SVC
+from sklearn.metrics import precision_recall_curve, auc
+from sklearn.svm import SVC
+
+
+
+def pr_linear_SVC(X_train, X_test, y_train, y_test):
+    clf = SVC(kernel="linear", random_state=0, tol=1e-5, class_weight="balanced", probability=True)
+    clf.fit(X_train, y_train)
+    y_scores = clf.predict_proba(X_test)
+    y_scores = y_scores[:, 1]
+    precision, recall, thresholds = precision_recall_curve(y_test, y_scores)
+    # calculate precision-recall AUC
+    return auc(recall, precision)
 
 
 if __name__ == '__main__':
